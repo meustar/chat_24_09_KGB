954247dc3c5c4fc725fbb1860f23fef53e54c12d
meustar <meustar829@gmail.com>
2024-09-06 01:19:40 +0900

    프로젝트 세팅

---
+++ src/main/java/com/koreait/exam/chat_24_09_kgb/Chat2409KgbApplication.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.chat_24_09_kgb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chat2409KgbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chat2409KgbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/exam/chat_24_09_kgb/Chat2409KgbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.chat_24_09_kgb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chat2409KgbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
